684c4d066a9c5d1af6301678ce2548c8efa7f28f
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2023-12-16 21:50:11 -0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,10 +11,9 @@ const route = require("./Views/route");
 const sport_route = require("./Views/sports");
 const ws = require("ws");
 const jwt = require("jsonwebtoken");
-const { userInfo } = require("./Controller/authController");
 const secret = process.env.SECRET;
 const User = require("./Model/User");
-const Message = require("./Model/Message");
+
 
 app.use(cors());
 app.use(express.json());
@@ -32,9 +31,6 @@ mongoose
     console.log(error);
   });
 
-//  app.listen(port, hostname, () => {
-//   console.log(`The app is running on ${hostname} ${port}`);
-// });
 
 const server = app.listen(port, hostname, () => {
   console.log(`The app is running on ${hostname} ${port}`);
@@ -42,57 +38,6 @@ const server = app.listen(port, hostname, () => {
 
 const wss = new ws.Server({ server });
 const onlineUsers = new Set();
-
-// wss.on('connection', async (connection, req) => {
-//   const cookies = req.headers.cookie;
-
-//   if (cookies) {
-//     const authTokenMatch = cookies.match(/authToken=([^;]*)/);
-//     const authToken = authTokenMatch ? authTokenMatch[1] : null;
-
-//     if (authToken) {
-//       try {
-//         const decodedToken = jwt.verify(authToken, secret);
-//         const userId = decodedToken.userId;
-
-//         const user = await User.findById(userId);
-
-//         if (user) {
-//           const { firstName } = user;
-
-//           connection.userId = userId;
-//           connection.username = firstName;
-
-//           onlineUsers.add(userId);
-
-//           broadcastOnlineUsers();
-
-//         }
-//       } catch (error) {
-//         console.error('Error decoding token:', error);
-//         connection.close();
-//       }
-//     }
-//   }
-//   connection.on('close', ()=>{
-//     if(connection.userId){
-//       onlineUsers.delete(connection.userId);
-//       broadcastOnlineUsers();
-//     }
-//   })
-// });
-
-// function broadcastOnlineUsers(){
-//   const onlineUsersArray = Array.from(onlineUsers);
-//   wss.clients.forEach((client)=>{
-//     if(client.readyState === ws.OPEN){
-//       client.send(JSON.stringify({
-//         type: "onlineUsers",
-//         data: onlineUsersArray
-//       }))
-//     }
-//   })
-// }
 
 // Define broadcastOnlineUsers outside the connection event handler
 function broadcastOnlineUsers() {
