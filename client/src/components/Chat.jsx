823bfb1ceab79111ef6a0fa37c45413ed6d1d87a
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2024-03-01 14:53:34 -0600

    Update Chat.jsx

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import { FaChevronDown, FaChevronUp, FaUsers } from "react-icons/fa";
 import Avatar from "../components/Avatar";
 
 const Chat = () => {
@@ -22,6 +22,7 @@ const Chat = () => {
   const [lastMessage, setLastMessage] = useState("");
   const [isSuggestionsOpen, setIsSuggestionsOpen] = useState(false);
   const [fetchMessages, setFetchMessages] = useState({});
+  const [isMobileUsersVisible, setIsMobileUsersVisible] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,43 +41,6 @@ const Chat = () => {
   useEffect(() => {
     connect();
   }, [uId]);
-
-  // const connect = () => {
-  //   if (uId) {
-  //     try {
-  //       const socket = new WebSocket("ws://localhost:5000/user");
-  //       setWs(socket);
-  //       if (!socket || !socket.addEventListener) {
-  //         console.error(
-  //           "Websocket is not available or does not support addEventListener"
-  //         );
-  //         return;
-  //       }
-  //       socket.onmessage = (e) => {
-  //         console.log("WebSocket message received:", e.data);
-  //         const receivedMessage = JSON.parse(e.data);
-  //         if ((receivedMessage.type = "onlineUsers")) {
-  //           setOnlineUsers(receivedMessage.data.map((user) => user.userId));
-  //           setOnlinePeople(receivedMessage.data);
-  //         }
-  //       };
-  //       socket.addEventListener("close", () => {
-  //         setTimeout(() => {
-  //           console.log("Disconnected... Reconnecting");
-  //           connect();
-  //         });
-  //       });
-  //       return () => {
-  //         if (socket && socket.close) {
-  //           socket.close();
-  //         }
-  //       };
-  //     } catch (error) {
-  //       console.error("Error creating WebSocket connection:", error);
-  //     }
-  //   }
-  // };
-
 
   const connect = () => {
     if (uId) {
@@ -99,7 +63,6 @@ const Chat = () => {
             } else if ("text" in messageData) {
               if (messageData.sender !== uId) {
                 setMessages((prev) => [...prev, { ...messageData }]);
-                // Check if the sender is not the currently selected user to increment the unread count
                 if (selectedUserIds !== messageData.sender) {
                   setUnreadCounts((prev) => ({
                     ...prev,
@@ -128,10 +91,6 @@ const Chat = () => {
       }
     }
   };
-  
-
-
-
 
   useEffect(() => {
     const fetchSearchSuggestions = async () => {
@@ -202,6 +161,7 @@ const Chat = () => {
         ...prev,
         [userId]: 0,
       }));
+      setIsMobileUsersVisible(false);
     } catch (error) {
       console.error("Error selecting contact:", error);
     }
@@ -230,7 +190,6 @@ const Chat = () => {
       console.error("Error sending message:", error);
     }
   }
-  
 
   useEffect(() => {
     if (!selectedUserIds) {
@@ -277,182 +236,150 @@ const Chat = () => {
       offlineUserData.find((user) => user._id === selectedUserIds)
     : null;
 
-  // const removeUserFromChat = (userId) => {
-  //   console.log("Removing User from Chat");
-  //   setFetchMessages((prev) => ({
-  //     ...prev,
-  //     [userId]: messages,
-  //   }));
-  //   setUserIds((prev) => prev.filter((id) => id !== userId));
-  //   const remainingUsers = userIds.filter((id) => id !== userId);
-  //   if (remainingUsers.length > 0) {
-  //     const nextUserId = remainingUsers[0];
-  //     setSelectedUserIds(nextUserId);
-  //     if (fetchMessages[nextUserId]) {
-  //       setMessages(fetchMessages[nextUserId]);
-  //     } else {
-  //       const fetchData = async () => {
-  //         try {
-  //           const response = await axios.get(`/user/messages/${nextUserId}`);
-  //           setMessages(response.data);
-  //         } catch (error) {
-  //           console.error("Error Fetching Message History:", error.message);
-  //         }
-  //       };
-  //       fetchData();
-  //     }
-  //   } else {
-  //     setMessages([]);
-  //     setSelectedUserIds(null);
-  //   }
-  // };
-
   return (
-    <div className="flex w-full h-[100vh] pt-16">
-      {/* <div className="bg-blue-100 md:w-2/5 w-2/3 flex flex-col h-full overflow-y-auto"> */}
-      <div className="bg-gray-800 md:w-2/5 w-2/3 flex flex-col h-full overflow-y-auto">
-        <div className="p-2 relative" ref={searchInputRef}>
-          <input
-            type="text"
-            placeholder="Search Users..."
-            className="w-full p-2 border rounded-lg focus:outline-none"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            onFocus={() => setIsSuggestionsOpen(true)}
-          />
-          {searchSuggestions.length > 0 && isSuggestionsOpen && (
-            <div className="absolute top-full left-0 bg-gray-700 border rounded-lg mt-1 w-full max-h-40 overflow-y-auto z-10 suggestion-dropdown">
-            {/* <div className="absolute top-full left-0 bg-white border rounded-lg mt-1 w-full max-h-40 overflow-y-auto z-10 suggestion-dropdown"> */}
-              {searchSuggestions.slice(0, 5).map((user) => (
-                <div
-                  key={user._id}
-                  className="p-2 cursor-pointer text-white hover:bg-black"
-                  onClick={() => handleSelectUserFromSearch(user._id)}
-                >
-                  {user.firstName}
-                </div>
-                // <div
-                //   key={user._id}
-                //   className="p-2 cursor-pointer hover:bg-gray-100"
-                //   onClick={() => handleSelectUserFromSearch(user._id)}
-                // >
-                //   {user.firstName}
-                // </div>
-              ))}
-            </div>
-          )}
-        </div>
-        <div className="p-2 w-full flex flex-col items-center justify-center">
-          <div className="font-bold md:border md:rounded-lg md:border-black w-full p-2 text-left mb-2 bg-transparent">
-            <div
-              className="flex h-full justify-between items-center text-white cursor-pointer"
-              onClick={() => setOnlineCollapsed(!onlineCollapsed)}
-            >
-              Online Users ({Math.max(0, onlineUsers.length - 1)})
-              {/* Ensures the count is never negative. It doesn't go below 0... it subtracts 1 from the length of 'onlineUsers' */}
-              {onlineCollapsed ? <FaChevronDown /> : <FaChevronUp />}
-            </div>
+    <div className="flex flex-col w-full h-[100vh] pt-16">
+      {isMobileUsersVisible ? null : (
+        <button
+          className="md:hidden absolute top-4 right-4 z-50 p-2 bg-gray-700 text-white rounded-full"
+          onClick={() => setIsMobileUsersVisible(true)}
+        >
+          <FaUsers />
+        </button>
+      )}
+      <div className="flex flex-row w-full h-full overflow-hidden">
+        <div className="bg-gray-800 md:w-2/5 w-full flex flex-col overflow-y-auto">
+          <div className="p-2 relative" ref={searchInputRef}>
+            <input
+              type="text"
+              placeholder="Search Users..."
+              className="w-full p-2 border rounded-lg focus:outline-none"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              onFocus={() => setIsSuggestionsOpen(true)}
+            />
+            {searchSuggestions.length > 0 && isSuggestionsOpen && (
+              <div className="absolute top-full left-0 bg-gray-700 border rounded-lg mt-1 w-full max-h-40 overflow-y-auto z-10 suggestion-dropdown">
+                {searchSuggestions.slice(0, 5).map((user) => (
+                  <div
+                    key={user._id}
+                    className="p-2 cursor-pointer text-white hover:bg-black"
+                    onClick={() => handleSelectUserFromSearch(user._id)}
+                  >
+                    {user.firstName}
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
-          {!onlineCollapsed && (
-            <div className="w-full overflow-y-auto md:border-black md:rounded-lg shadow-md bg-transparent">
-              <div className="p-2 text-left h-full overflow-y-auto">
-                {onlinePeople
-                  .filter((user) => user.userId !== uId)
-                  .map((user) => (
+          <div className="p-2 w-full flex flex-col items-center justify-center">
+            <div className="font-bold md:border md:rounded-lg md:border-black w-full p-2 text-left mb-2 bg-transparent">
+              <div
+                className="flex h-full justify-between items-center text-white cursor-pointer"
+                onClick={() => setOnlineCollapsed(!onlineCollapsed)}
+              >
+                Online Users ({Math.max(0, onlineUsers.length - 1)})
+                {onlineCollapsed ? <FaChevronDown /> : <FaChevronUp />}
+              </div>
+            </div>
+            {!onlineCollapsed && (
+              <div className="w-full overflow-y-auto md:border-black md:rounded-lg shadow-md bg-transparent">
+                <div className="p-2 text-left h-full overflow-y-auto">
+                  {onlinePeople
+                    .filter((user) => user.userId !== uId)
+                    .map((user) => (
+                      <div
+                        onClick={() => selectContact(user.userId)}
+                        key={user._id}
+                        className="p-2 text-white hover:bg-black transition duration-300 flex items-center"
+                      >
+                        <div>
+                          <Avatar
+                            online={true}
+                            username={user.username}
+                            userId={user.userId}
+                            unreadCount={unreadCounts[user.userId] || 0}
+                          />
+                        </div>
+                        <h1 className="capitalize text-white">
+                          {user.username}
+                        </h1>
+                        {selectedUserIds === user.userId && (
+                          <button
+                            className="ml-auto p-2 text-red-500 focus:outline-none"
+                            onClick={() => removeUserFromChat(user.userId)}
+                          >
+                            x
+                          </button>
+                        )}
+                      </div>
+                    ))}
+                </div>
+              </div>
+            )}
+            <div className="font-bold md:border md:rounded-lg md:border-black w-full p-2 text-left mb-2 bg-transparent">
+              <div
+                className="flex justify-between items-center cursor-pointer"
+                onClick={() => setOfflineCollapsed(!offlineCollapsed)}
+              >
+                <h1 className="p-2 text-white">
+                  Offline Users ({offlineUserData.length})
+                </h1>
+                {offlineCollapsed ? <FaChevronDown /> : <FaChevronUp />}
+              </div>
+            </div>
+            {!offlineCollapsed && (
+              <div className="w-full overflow-y-auto md:border-black md:rounded-lg shadow-md bg-transparent">
+                <div className="p-2 text-left text-white h-[30%] overflow-y-auto">
+                  {offlineUserData.map((user) => (
                     <div
-                      onClick={() => selectContact(user.userId)}
                       key={user._id}
-                      className="p-2 text-white hover:bg-black transition duration-300 flex items-center"
+                      className="p-2 hover:bg-black transition duration-300 flex items-center"
+                      onClick={() => selectContact(user._id)}
                     >
                       <div>
                         <Avatar
-                          online={true}
-                          username={user.username}
-                          userId={user.userId}
-                          unreadCount={unreadCounts[user.userId] || 0}
+                          online={false}
+                          username={user.firstName}
+                          userId={user._id}
+                          unreadCount={unreadCounts[user._id] || 0}
                         />
                       </div>
-                      <h1 className="capitalize text-white">{user.username}</h1>
-                      {selectedUserIds === user.userId && (
+                      <h1 className="capitalize text-white">
+                        {user.firstName}
+                      </h1>
+                      {selectedUserIds === user._id && (
                         <button
                           className="ml-auto p-2 text-red-500 focus:outline-none"
-                          // onClick={() => removeUserFromChat(user.userId)}
+                          onClick={() => removeUserFromChat(user._id)}
                         >
                           x
                         </button>
                       )}
                     </div>
                   ))}
+                </div>
               </div>
-            </div>
-          )}
-
-          <div className="font-bold md:border md:rounded-lg md:border-black w-full p-2 text-left mb-2 bg-transparent">
-            <div
-              className="flex justify-between items-center cursor-pointer"
-              onClick={() => setOfflineCollapsed(!offlineCollapsed)}
-            >
-              <h1 className="p-2 text-white">Offline Users ({offlineUserData.length})</h1>
-              {offlineCollapsed ? <FaChevronDown /> : <FaChevronUp />}
-            </div>
+            )}
           </div>
-          {!offlineCollapsed && (
-            <div className="w-full overflow-y-auto md:border-black md:rounded-lg shadow-md bg-transparent">
-              <div className="p-2 text-left text-white h-[30%] overflow-y-auto">
-                {offlineUserData.map((user) => (
-                  <div
-                    key={user._id}
-                    className="p-2  hover:bg-black transition duration-300 flex items-center"
-                    onClick={() => selectContact(user._id)}
-                  >
-                    <div>
-                      <Avatar
-                        online={false}
-                        username={user.firstName}
-                        userId={user._id}
-                        unreadCount={unreadCounts[user._id] || 0}
-                      />
-                    </div>
-                    <h1 className="capitalize text-white">{user.firstName}</h1>
-                    {selectedUserIds === user._id && (
-                      <button
-                        className="ml-auto p-2 text-red-500 focus:outline-none"
-                        onClick={() => removeUserFromChat(user._id)}
-                      >
-                        x
-                      </button>
-                    )}
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
         </div>
-        {/* <div className="p-2 text-center mt-auto">
-          <h1 className="capitalize">{user}</h1>
-        </div> */}
-      </div>
-      <div className="md:w-3/5 w-3/5 flex flex-col h-full bg-gray-700">
-      {/* <div className="md:w-3/5 w-3/5 flex flex-col h-full bg-blue-300"> */}
-        {selectedUser && (
-          <div className="w-full border-b p-3 border-black text-white rounded-lg">
-            <div className="flex items-center">
-              <Avatar
-                online={onlineUsers.includes(
-                  selectedUser._id || selectedUser.userId
-                )}
-                username={selectedUser?.username || selectedUser?.firstName}
-                userId={selectedUser._id || selectedUser.userId}
-              />
-              <h1 className="font-bold capitalize">
-                {selectedUser?.username || selectedUser?.firstName}
-              </h1>
+        <div className="md:w-3/5 w-full flex flex-col h-full bg-gray-700">
+          {selectedUser && (
+            <div className="w-full border-b p-3 border-black text-white rounded-lg">
+              <div className="flex items-center">
+                <Avatar
+                  online={onlineUsers.includes(
+                    selectedUser._id || selectedUser.userId
+                  )}
+                  username={selectedUser?.username || selectedUser?.firstName}
+                  userId={selectedUser._id || selectedUser.userId}
+                />
+                <h1 className="font-bold capitalize">
+                  {selectedUser?.username || selectedUser?.firstName}
+                </h1>
+              </div>
             </div>
-          </div>
-        )}
-
-        {selectedUserIds && (
-          <div id="chat-Container" className="flex-grow overflow-y-auto p-4">
+          )}
+          <div className="flex-grow overflow-y-auto p-4" id="chat-Container">
             {Array.isArray(messages) && messages.length > 0
               ? messages.map((message, index) => (
                   <div
@@ -464,43 +391,46 @@ const Chat = () => {
                   >
                     <div
                       className={
-                        "max-w-full md:p-4 p-3 py-2 mb-2 rounded-lg " +
+                        "max-w-full md:max-w-xs lg:max-w-md xl:max-w-lg p-3 py-2 mb-2 rounded-lg " +
                         (message.sender === uId
                           ? "bg-blue-500 text-white"
-                          : "bg-white text-gray-500")
+                          : "bg-white text-gray-800")
                       }
                     >
                       <h1>{message.text}</h1>
-                      <div className="md:text-[1.3vmin] text-[1.6vmin] text-gray-400">
+                      <div className="text-[0.75rem] text-gray-400">
                         {formatTimeStamp(message.createdAt)}
                       </div>
                     </div>
                   </div>
                 ))
-              : null}
+              :
+              <div className="w-full h-full flex justify-center items-center">
+              <p className="font-semibold capitalize">"No Message History"</p>
+              </div>
+              }
           </div>
-        )}
-        {selectedUserIds && (
-          <div className="p-4 flex items-center rounded-lg w-full relative">
-            <input
-              type="text"
-              value={newMessageText}
-              onChange={(ev) => setNewMessageText(ev.target.value)}
-              placeholder="Type Your Message Here"
-              className="flex-grow p-2 text-black border rounded-lg focus:outline-none"
-            />
-            <button
-              type="submit"
-              className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-blue-500 p-2 border rounded-lg text-white"
-              onClick={sendMessage}
-            >
-              Send
-            </button>
-          </div>
-        )}
+          {selectedUserIds && (
+            <div className="mt-auto p-4 flex items-center rounded-lg w-full bg-white">
+              <input
+                type="text"
+                value={newMessageText}
+                onChange={(ev) => setNewMessageText(ev.target.value)}
+                placeholder="Type Your Message Here"
+                className="flex-grow p-2 text-black border rounded-lg focus:outline-none"
+              />
+              <button
+                type="submit"
+                className="ml-4 bg-blue-500 p-2 border rounded-lg text-white"
+                onClick={sendMessage}
+              >
+                Send
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
 };
-
 export default Chat;
