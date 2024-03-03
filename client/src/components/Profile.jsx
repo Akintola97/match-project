c670bf9ea45c795775f1fab04b9cd5d8d40d677a
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2024-03-02 21:31:22 -0600

    Update Profile.jsx

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -101,24 +101,189 @@ const Profile = () => {
     setSelectedDays(updatedSelectedDays);
   };
 
+  //   return (
+  //     <div className="flex flex-col h-full max-w-4xl mx-auto">
+  //       <form
+  //         className="bg-white shadow-lg rounded-lg p-8 mt-10"
+  //         onSubmit={handleSubmit}
+  //       >
+  //         <h1 className="text-3xl font-bold text-center text-gray-800 mb-10">
+  //           Profile Settings
+  //         </h1>
+  //         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+  //           <div className="mb-6">
+  //             <label className="block mb-2 text-gray-600 font-medium">
+  //               First Name:
+  //             </label>
+  //             <div className="flex items-center">
+  //               <UserCircleIcon className="h-5 w-5 text-gray-400 mr-2" />
+  //               <input
+  //                 className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+  //                 type="text"
+  //                 placeholder="First Name"
+  //                 value={firstName}
+  //                 onChange={(e) => setFirstName(e.target.value)}
+  //                 required
+  //               />
+  //             </div>
+  //           </div>
+  //           <div className="mb-6">
+  //             <label className="block mb-2 text-gray-600 font-medium">
+  //               Email:
+  //             </label>
+  //             <div className="flex items-center">
+  //               <MailIcon className="h-5 w-5 text-gray-400 mr-2" />
+  //               <input
+  //                 className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+  //                 type="email"
+  //                 placeholder="Email"
+  //                 value={email}
+  //                 onChange={(e) => setEmail(e.target.value)}
+  //                 required
+  //               />
+  //             </div>
+  //           </div>
+  //           <div className="mb-6">
+  //             <label className="block mb-2 text-gray-600 font-medium">
+  //               Alternate Email:
+  //             </label>
+  //             <div className="flex items-center">
+  //               <MailIcon className="h-5 w-5 text-gray-400 mr-2" />
+  //               <input
+  //                 className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+  //                 type="email"
+  //                 placeholder="Alternate Email"
+  //                 value={newBackupEmail}
+  //                 onChange={(e) => setNewBackupEmail(e.target.value)}
+  //                 required
+  //               />
+  //             </div>
+  //           </div>
+  //           <div className="mb-6">
+  //             <label className="block mb-2 text-gray-600 font-medium">
+  //               Birthdate:
+  //             </label>
+  //             <div className="flex items-center">
+  //               <CalendarIcon className="h-5 w-5 text-gray-400 mr-2" />
+  //               <input
+  //                 type="date"
+  //                 className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+  //                 value={birthdate}
+  //                 onChange={(e) => setBirthdate(e.target.value)}
+  //                 required
+  //               />
+  //             </div>
+  //           </div>
+  //           <div className="mb-6">
+  //             <label className="block mb-2 text-gray-600 font-medium">
+  //               Gender:
+  //             </label>
+  //             <div className="flex items-center">
+  //               <ChevronDownIcon className="h-5 w-5 text-gray-400 mr-2" />
+  //               <select
+  //                 className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+  //                 value={gender}
+  //                 onChange={(e) => setGender(e.target.value)}
+  //                 required
+  //               >
+  //                 <option value="">Select Gender</option>
+  //                 <option value="Male">Male</option>
+  //                 <option value="Female">Female</option>
+  //                 <option value="Other">Other</option>
+  //               </select>
+  //             </div>
+  //           </div>
+  //           <div className="mb-6">
+  //             <label className="block mb-2 text-gray-600 font-medium">
+  //               Rating:
+  //             </label>
+  //             <div className="flex items-center">
+  //               <StarIcon className="h-5 w-5 text-gray-400 mr-2" />
+  //               <select
+  //                 className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+  //                 value={rating}
+  //                 onChange={(e) => setRating(e.target.value)}
+  //                 required
+  //               >
+  //                 <option value="">Select Rating</option>
+  //                 <option value="1.5-2.0">1.5-2.0</option>
+  //                 <option value="2.5-3.0">2.5-3.0</option>
+  //                 <option value="3.5-4.0">3.5-4.0</option>
+  //                 <option value="4.5+">4.5+</option>
+  //               </select>
+  //             </div>
+  //           </div>
+  //           <div className="mb-6 col-span-2">
+  //             <label className="block mb-2 text-gray-600 font-medium">
+  //               Days Available to Play:
+  //             </label>
+  //             <div className="flex flex-wrap">
+  //               {daysOfWeek.map((day, index) => (
+  //                 <label key={index} className="flex items-center m-2">
+  //                   <input
+  //                     type="checkbox"
+  //                     value={day}
+  //                     checked={selectedDays.includes(day)}
+  //                     onChange={() => handleDayChange(day)}
+  //                     className="mr-2"
+  //                   />
+  //                   {day}
+  //                 </label>
+  //               ))}
+  //             </div>
+  //           </div>
+  //           <div className="mb-6 col-span-2">
+  //             <label className="block mb-2 text-gray-600 font-medium">
+  //               Time Looking to Play:
+  //             </label>
+  //             <div className="flex items-center">
+  //               <ClockIcon className="h-5 w-5 text-gray-400 mr-2" />
+  //               <select
+  //                 className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+  //                 name="timeToPlay"
+  //                 value={timeToPlay}
+  //                 onChange={(e) => setTimeToPlay(e.target.value)}
+  //                 required
+  //               >
+  //                 <option value="">Select Time</option>
+  //                 <option value="morning">6-11am</option>
+  //                 <option value="afternoon">12-4pm</option>
+  //                 <option value="evening">5-9pm</option>
+  //               </select>
+  //             </div>
+  //           </div>
+  //         </div>
+  //         <div className="mt-6">
+  //           <button
+  //             className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-4 rounded-lg transition duration-200"
+  //             type="submit"
+  //           >
+  //             Save Profile
+  //           </button>
+  //         </div>
+  //       </form>
+  //     </div>
+  //   );
+  // };
+
+  // export default Profile;
+
   return (
-    <div className="flex flex-col h-full max-w-4xl mx-auto">
+    <div className="flex flex-col h-full w-full mx-auto bg-gray-900 text-gray-200">
       <form
-        className="bg-white shadow-lg rounded-lg p-8 mt-10"
+        className="bg-gray-800 shadow-lg rounded-lg p-8 mt-10"
         onSubmit={handleSubmit}
       >
-        <h1 className="text-3xl font-bold text-center text-gray-800 mb-10">
+        <h1 className="text-3xl font-bold text-center mb-10">
           Profile Settings
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           <div className="mb-6">
-            <label className="block mb-2 text-gray-600 font-medium">
-              First Name:
-            </label>
+            <label className="block mb-2 font-medium">First Name:</label>
             <div className="flex items-center">
-              <UserCircleIcon className="h-5 w-5 text-gray-400 mr-2" />
+              <UserCircleIcon className="h-5 w-5 mr-2 text-gray-400" />
               <input
-                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500 bg-gray-700 text-white"
                 type="text"
                 placeholder="First Name"
                 value={firstName}
@@ -128,13 +293,11 @@ const Profile = () => {
             </div>
           </div>
           <div className="mb-6">
-            <label className="block mb-2 text-gray-600 font-medium">
-              Email:
-            </label>
+            <label className="block mb-2 font-medium">Email:</label>
             <div className="flex items-center">
-              <MailIcon className="h-5 w-5 text-gray-400 mr-2" />
+              <MailIcon className="h-5 w-5 mr-2 text-gray-400" />
               <input
-                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500 bg-gray-700 text-white"
                 type="email"
                 placeholder="Email"
                 value={email}
@@ -144,13 +307,11 @@ const Profile = () => {
             </div>
           </div>
           <div className="mb-6">
-            <label className="block mb-2 text-gray-600 font-medium">
-              Alternate Email:
-            </label>
+            <label className="block mb-2 font-medium">Alternate Email:</label>
             <div className="flex items-center">
-              <MailIcon className="h-5 w-5 text-gray-400 mr-2" />
+              <MailIcon className="h-5 w-5 mr-2 text-gray-400" />
               <input
-                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500 bg-gray-700 text-white"
                 type="email"
                 placeholder="Alternate Email"
                 value={newBackupEmail}
@@ -160,14 +321,12 @@ const Profile = () => {
             </div>
           </div>
           <div className="mb-6">
-            <label className="block mb-2 text-gray-600 font-medium">
-              Birthdate:
-            </label>
+            <label className="block mb-2 font-medium">Birthdate:</label>
             <div className="flex items-center">
-              <CalendarIcon className="h-5 w-5 text-gray-400 mr-2" />
+              <CalendarIcon className="h-5 w-5 mr-2 text-gray-400" />
               <input
                 type="date"
-                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500 bg-gray-700 text-white"
                 value={birthdate}
                 onChange={(e) => setBirthdate(e.target.value)}
                 required
@@ -175,13 +334,11 @@ const Profile = () => {
             </div>
           </div>
           <div className="mb-6">
-            <label className="block mb-2 text-gray-600 font-medium">
-              Gender:
-            </label>
+            <label className="block mb-2 font-medium">Gender:</label>
             <div className="flex items-center">
-              <ChevronDownIcon className="h-5 w-5 text-gray-400 mr-2" />
+              <ChevronDownIcon className="h-5 w-5 mr-2 text-gray-400" />
               <select
-                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500 bg-gray-700 text-white"
                 value={gender}
                 onChange={(e) => setGender(e.target.value)}
                 required
@@ -194,13 +351,11 @@ const Profile = () => {
             </div>
           </div>
           <div className="mb-6">
-            <label className="block mb-2 text-gray-600 font-medium">
-              Rating:
-            </label>
+            <label className="block mb-2 font-medium">Rating:</label>
             <div className="flex items-center">
-              <StarIcon className="h-5 w-5 text-gray-400 mr-2" />
+              <StarIcon className="h-5 w-5 mr-2 text-gray-400" />
               <select
-                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500 bg-gray-700 text-white"
                 value={rating}
                 onChange={(e) => setRating(e.target.value)}
                 required
@@ -214,7 +369,7 @@ const Profile = () => {
             </div>
           </div>
           <div className="mb-6 col-span-2">
-            <label className="block mb-2 text-gray-600 font-medium">
+            <label className="block mb-2 font-medium">
               Days Available to Play:
             </label>
             <div className="flex flex-wrap">
@@ -233,13 +388,13 @@ const Profile = () => {
             </div>
           </div>
           <div className="mb-6 col-span-2">
-            <label className="block mb-2 text-gray-600 font-medium">
+            <label className="block mb-2 font-medium">
               Time Looking to Play:
             </label>
             <div className="flex items-center">
-              <ClockIcon className="h-5 w-5 text-gray-400 mr-2" />
+              <ClockIcon className="h-5 w-5 mr-2 text-gray-400" />
               <select
-                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500"
+                className="w-full border rounded-lg py-2 px-4 leading-tight focus:outline-none focus:border-blue-500 bg-gray-700 text-white"
                 name="timeToPlay"
                 value={timeToPlay}
                 onChange={(e) => setTimeToPlay(e.target.value)}
@@ -255,7 +410,7 @@ const Profile = () => {
         </div>
         <div className="mt-6">
           <button
-            className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-4 rounded-lg transition duration-200"
+            className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-lg transition duration-200"
             type="submit"
           >
             Save Profile
