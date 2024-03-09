d101173e0a7f0d5b7057f8c293a9ca596dd1112b
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2024-03-08 20:46:05 -0600

    update

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,32 +1,86 @@
-import React from 'react';
-import { useCart } from '../CartContext';
+import React from "react";
+import { useCart } from "../CartContext";
+import { useSpring, animated } from "react-spring";
 
 const Cart = () => {
   const { cartItems, removeFromCart, addToCart } = useCart();
 
+  // Spring animation for the cart items
+  const props = useSpring({
+    to: { opacity: 1 }, // Corrected to fade in
+    from: { opacity: 0 },
+    reset: false,
+    config: { duration: 500 },
+  });
+
   return (
-    <div className="w-full min-h-screen pt-[10vh]">
-      <h2>Your Cart</h2>
+    <animated.div
+      style={props}
+      className="w-full min-h-screen pt-[10vh] bg-gray-900"
+    >
+      <h1 className="w-full text-center font-semibold text-[5vmin] text-white">
+        Cart
+      </h1>
       {Object.keys(cartItems).length === 0 ? (
-        <p>Your cart is empty</p>
+        <div className="w-full h-[50vh]">
+          <p className="w-full h-full flex justify-center text-[3vmin] items-center font-semibold text-white">
+            Your cart is empty
+          </p>
+        </div>
       ) : (
-        <div>
+        <div className="w-full h-full grid grid-cols-1 md:grid-cols-3 flex-wrap p-5 gap-4">
           {Object.entries(cartItems).map(([itemId, itemDetails]) => (
-            <div key={itemId} className="cart-item">
-              <img src={itemDetails.imageUrl} alt={itemDetails.name} style={{ width: '100px' }} />
-              <h4>{itemDetails.name}</h4>
-              <p>Price: ${itemDetails.price}</p>
-              <p>Quantity: {itemDetails.quantity}</p>
-              <div className="cart-item-actions">
-                <button onClick={() => addToCart(itemDetails, 1)}>+</button>
-                <button onClick={() => removeFromCart(itemId)}>Remove</button>
-                <button onClick={() => removeFromCart(itemId, true)}>-</button>
+            <animated.div
+              key={itemId}
+              style={props}
+              className="bg-white p-3 m-1 rounded-2xl shadow-2xl border border-green-500"
+            >
+              <img
+                src={itemDetails.imageUrl}
+                alt={itemDetails.name}
+                style={{ width: "100px", margin: "auto", display: "block" }}
+              />
+              <h4 className="text-xl font-bold mb-2 text-center capitalize p-2 text-green-800">
+                {itemDetails.name}
+              </h4>
+              <p className="text-center text-green-800">
+                Price: ${itemDetails.price}
+              </p>
+              <p className="text-center text-green-800">
+                Quantity: {itemDetails.quantity}
+              </p>
+              <div className="cart-item-actions flex justify-center">
+                <button
+                  onClick={() => addToCart(itemDetails, 1)}
+                  className="m-1 bg-green-500 text-white font-bold py-2 px-4 rounded"
+                >
+                  +
+                </button>
+                <button
+                  onClick={() => removeFromCart(itemId)}
+                  className="m-1 bg-red-500 text-white font-bold py-2 px-4 rounded"
+                >
+                  Remove
+                </button>
+                <button
+                  onClick={() => {
+                    const currentQuantity = itemDetails.quantity;
+                    if (currentQuantity > 1) {
+                      addToCart(itemDetails, -1);
+                    } else if (currentQuantity === 1) {
+                      removeFromCart(itemId);
+                    }
+                  }}
+                  className="m-1 bg-blue-500 text-white font-bold py-2 px-4 rounded"
+                >
+                  -
+                </button>
               </div>
-            </div>
+            </animated.div>
           ))}
         </div>
       )}
-    </div>
+    </animated.div>
   );
 };
 
--- client/src/components/Store.jsx
+++ client/src/components/Store.jsx
@@ -14,14 +14,13 @@ import {
   TextField,
   Select,
   MenuItem,
-  Container
 } from "@mui/material";
-import { useCart } from '../CartContext';
+import { useCart } from "../CartContext";
 
 const Store = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
-  const [sortOption, setSortOption] = useState('');
+  const [sortOption, setSortOption] = useState("");
   const [categoryFilters, setCategoryFilters] = useState({
     clothing: false,
     rackets: false,
@@ -57,13 +56,19 @@ const Store = () => {
   };
 
   const sortedAndFilteredData = () => {
-    const filters = Object.entries(categoryFilters).filter(([_, value]) => value).map(([key, _]) => key);
-    let result = filters.length === 0 ? data : data.filter(item => filters.includes(item.category));
+    const filters = Object.entries(categoryFilters)
+      .filter(([_, value]) => value)
+      .map(([key, _]) => key);
+    let result =
+      filters.length === 0
+        ? data
+        : data.filter((item) => filters.includes(item.category));
 
     if (searchTerm) {
-      result = result.filter(item =>
-        item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        item.description.toLowerCase().includes(searchTerm.toLowerCase())
+      result = result.filter(
+        (item) =>
+          item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.description.toLowerCase().includes(searchTerm.toLowerCase())
       );
     }
 
@@ -81,9 +86,17 @@ const Store = () => {
 
   return (
     <div className="w-full min-h-screen pt-[10vh] bg-gray-900">
-      <Box sx={{ display: 'flex', p: 2, flexDirection: { xs: 'column', md: 'row' } }}>
-        <Box sx={{ width: { xs: '100%', md: '25%' }, color: 'white' }}>
-          <Typography variant="h6" marginBottom={2}>Filter by Category:</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          p: 2,
+          flexDirection: { xs: "column", md: "row" },
+        }}
+      >
+        <Box sx={{ width: { xs: "100%", md: "25%" }, color: "white" }}>
+          <Typography variant="h6" marginBottom={2}>
+            Filter by Category:
+          </Typography>
           <FormGroup>
             {Object.keys(categoryFilters).map((category) => (
               <FormControlLabel
@@ -94,20 +107,31 @@ const Store = () => {
                     onChange={handleCategoryChange}
                     name={category}
                     sx={{
-                      color: 'white',
-                      '&.Mui-checked': {
-                        color: 'white',
+                      color: "white",
+                      "&.Mui-checked": {
+                        color: "white",
                       },
                     }}
                   />
                 }
-                label={<Typography>{category.charAt(0).toUpperCase() + category.slice(1)}</Typography>}
+                label={
+                  <Typography>
+                    {category.charAt(0).toUpperCase() + category.slice(1)}
+                  </Typography>
+                }
               />
             ))}
           </FormGroup>
         </Box>
-        <Box sx={{ width: { xs: '100%', md: '75%' }, p: 2 }}>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 2 }}>
+        <Box sx={{ width: { xs: "100%", md: "75%" }, p: 2 }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              mb: 2,
+            }}
+          >
             <TextField
               fullWidth
               label="Search..."
@@ -116,78 +140,124 @@ const Store = () => {
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
               InputLabelProps={{
-                style: { color: 'white' },
+                style: { color: "white" },
               }}
               inputProps={{
-                style: { color: 'white' },
+                style: { color: "white" },
               }}
-              sx={{ bgcolor: 'grey.800', borderRadius: 1 }}
+              sx={{ bgcolor: "grey.800", borderRadius: 1 }}
             />
             <Select
               value={sortOption}
               onChange={handleSortChange}
               displayEmpty
-              sx={{ '.MuiSelect-select': { bgcolor: 'grey.800', color: 'white' }, width: { xs: '100%', sm: 'auto' }, ml: 2, borderRadius: 1 }}
+              sx={{
+                ".MuiSelect-select": { bgcolor: "grey.800", color: "white" },
+                width: { xs: "100%", sm: "auto" },
+                ml: 2,
+                borderRadius: 1,
+              }}
             >
-              <MenuItem value="" disabled>Sort By</MenuItem>
+              <MenuItem value="" disabled>
+                Sort By
+              </MenuItem>
               <MenuItem value="name">Name</MenuItem>
               <MenuItem value="priceLowHigh">Price: Low to High</MenuItem>
               <MenuItem value="priceHighLow">Price: High to Low</MenuItem>
             </Select>
           </Box>
           {loading ? (
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-              <Box sx={{ border: '4px solid green', borderRadius: '50%', width: 100, height: 100, animation: 'spin 2s linear infinite' }} />
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+              }}
+            >
+              <Box
+                sx={{
+                  border: "4px solid green",
+                  borderRadius: "50%",
+                  width: 100,
+                  height: 100,
+                  animation: "spin 2s linear infinite",
+                }}
+              />
             </Box>
           ) : (
             <Grid container spacing={3}>
-              {sortedAndFilteredData().length > 0 ? sortedAndFilteredData().map((item) => (
-                <Grid item key={item._id} xs={12} sm={6} md={4}>
-                  <Card sx={{ bgcolor: 'grey.800' }}>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={item.imageUrl}
-                      alt={item.name}
-                    />
-                    <CardContent className="text-white capitalize">
-                      <Typography gutterBottom variant="h5" component="div">
-                        {item.name}
-                      </Typography>
-                      <Typography>
-                        {item.description}
-                      </Typography>
-                      <Typography marginTop={2}>
-                        Price: ${item.price}
-                      </Typography>
-                      <Typography marginTop={1}>
-                        Category: {item.category}
-                      </Typography>
-                      <Box sx={{ display: 'flex', alignItems: 'center', mt: 2 }}>
-                        <Button
-                          variant="contained"
-                          onClick={() => removeFromCart(item._id)}
-                          sx={{ bgcolor: 'grey.700', color: 'white', minWidth: '36px' }}
-                        >
-                          -
-                        </Button>
-                        <Typography sx={{ marginX: 2, color: 'white' }}>
-                          {cartItems[item._id]?.quantity || 0}
+              {sortedAndFilteredData().length > 0 ? (
+                sortedAndFilteredData().map((item) => (
+                  <Grid item key={item._id} xs={12} sm={6} md={4}>
+                    <Card sx={{ bgcolor: "grey.800" }}>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image={item.imageUrl}
+                        alt={item.name}
+                      />
+                      <CardContent className="text-white capitalize">
+                        <Typography gutterBottom variant="h5" component="div">
+                          {item.name}
                         </Typography>
-                        <Button
-                          variant="contained"
-                          onClick={() => addToCart(item, 1)} // Assuming addToCart takes item and quantity
-                          sx={{ bgcolor: 'grey.700', color: 'white', minWidth: '36px' }}
+                        <Typography>{item.description}</Typography>
+                        <Typography marginTop={2}>
+                          Price: ${item.price}
+                        </Typography>
+                        <Typography marginTop={1}>
+                          Category: {item.category}
+                        </Typography>
+                        <Box
+                          sx={{ display: "flex", alignItems: "center", mt: 2 }}
                         >
-                          +
-                        </Button>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              )) : (
+                          <Button
+                            variant="contained"
+                            onClick={() => {
+                              const currentQuantity =
+                                cartItems[item._id]?.quantity || 0;
+                              if (currentQuantity > 1) {
+                                // Decrease the item's quantity by one if greater than one
+                                addToCart(item, -1); // Adjust if your function logic is different
+                              } else if (currentQuantity === 1) {
+                                // Remove the item from the cart if the quantity is exactly one
+                                removeFromCart(item._id);
+                              }
+                              // Do nothing if currentQuantity is 0, effectively stopping at 0
+                            }}
+                            sx={{
+                              bgcolor: "grey.700",
+                              color: "white",
+                              minWidth: "36px",
+                            }}
+                          >
+                            -
+                          </Button>
+
+                          <Typography sx={{ marginX: 2, color: "white" }}>
+                            {cartItems[item._id]?.quantity || 0}
+                          </Typography>
+                          <Button
+                            variant="contained"
+                            onClick={() => addToCart(item, 1)} // Assuming addToCart takes item and quantity
+                            sx={{
+                              bgcolor: "grey.700",
+                              color: "white",
+                              minWidth: "36px",
+                            }}
+                          >
+                            +
+                          </Button>
+                        </Box>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))
+              ) : (
                 <Grid item xs={12}>
-                  <Typography variant="h6" textAlign="center" color="white">No items found.</Typography>
+                  <Typography variant="h6" textAlign="center" color="white">
+                    No items found.
+                  </Typography>
                 </Grid>
               )}
             </Grid>
