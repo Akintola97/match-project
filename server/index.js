e80f68f2f3d974c87b91b196bf02f6514f050617
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2024-03-31 20:19:46 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const hostname = "localhost";
-const port = 5001;
+const port = 10001;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
