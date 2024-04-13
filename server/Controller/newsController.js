dbe451de608f552074488dcf88a663d43ad30029
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2024-04-12 23:07:17 -0500

    Update newsController.js

--- server/Controller/newsController.js
+++ server/Controller/newsController.js
@@ -5,25 +5,22 @@ require("dotenv").config();
 exports.news_trending = async (req, res) => {
   try {
     const trending_data = await axios.get(
-      //`https://newsdata.io/api/1/news?qInTitle=tennis&category=sports&language=en&image=1&full_content=1&apikey=${apiKey}`
-      `https://newsdata.io/api/1/news?qInTitle=tennis&category=sports&language=en&image=1&full_content=1&size=9&apikey=${apiKey}`
+      `https://newsdata.io/api/1/news?qInTitle=tennis&category=sports&language=en&image=1&size=9&apikey=${apiKey}`
     );
     const response = trending_data.data;
     res.send(response);
-
   } catch (error) {
     console.error(error);
     res.status(500);
   }
 };
 
-
 exports.search = async (req, res) => {
   const { search } = req.body;
   try {
     const search_data = await axios.get(
       // `https://newsdata.io/api/1/news?apikey=${apiKey}&qInTitle=${search}&category=sports&language=en&image=1&full_content=1`
-      `https://newsdata.io/api/1/news?apikey=${apiKey}&qInTitle=${search}&category=sports&language=en&image=1&size=9&full_content=1`
+      `https://newsdata.io/api/1/news?apikey=${apiKey}&qInTitle=${search}&category=sports&language=en&image=1&size=9`
     );
     res.send(search_data.data);
   } catch (error) {
