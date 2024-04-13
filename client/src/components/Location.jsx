9665b2320d24215f609265f980790b490df620d6
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2024-04-12 22:06:26 -0500

    Update Location.jsx

--- client/src/components/Location.jsx
+++ client/src/components/Location.jsx
@@ -76,8 +76,8 @@ const Location = () => {
 
   if (loading) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        Loading...
+      <div className="flex justify-center items-center h-screen bg-gray-900">
+         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-green-500"></div>
       </div>
     );
   }
